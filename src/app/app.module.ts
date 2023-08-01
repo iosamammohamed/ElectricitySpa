c27f2ab0c281091ccbd3f8b11e383795e80cd9fb
gatewaydev\omagdy <gatewaydev\omagdy>
2023-08-01 12:28:13 +0300

    - fix versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,26 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { EndPointsService } from './home/services/end-points.service';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgbModule,
     BrowserModule,
     AppRoutingModule,
     NgxPaginationModule,
     AutocompleteLibModule,
-    HttpClientModule
+    HttpClientModule,
+    NgMultiSelectDropDownModule.forRoot()
   ],
   providers: [
      //Load json Files on startup
